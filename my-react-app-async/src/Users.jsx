0be4ecbbe142403738a3f510d7e9c4ed885506ca
GitMojoe <greatestmojoe.com>
2025-11-16 09:50:10 +0000

    Users.jsx the component that will be consuming the data in UserStore is implemented

---
+++ my-react-app-async/src/Users.jsx
@@ -0,0 +1,20 @@
+import { useUserStore } from "./Userstore";
+import { useEffect } from "react";
+
+export default function Users(){
+    const {users, loading, error, fetchUsers}=useUserStore();
+    useEffect(()=>{
+        fetchUsers();
+    },[])
+    if(loading) return <h3>loading users...</h3>
+    if(error) return <h3>{error}</h3>
+    return(
+        <div>
+            <h2>Users List</h2>
+            {users.map((user)=>(
+                <p key={user.id}>{user.name}</p>
+            ))}
+        </div>
+    );
+
+}
